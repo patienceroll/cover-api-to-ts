d365fa0473d07e55a272d8f0f24d7b670cdf0a9d
zhangxianlei <zhangxianlei@uphicoo.com>
2023-05-25 17:48:12 +0800

    feat: 🎸 init

---
+++ index.ts
@@ -0,0 +1,53 @@
+import fs from "fs/promises";
+import path from "path";
+
+const map = new Map<string, Sechma>();
+
+function parse(file: string, path: string) {
+  fs.readFile(file).then((res) => {
+    const json: OpenApi = JSON.parse(res.toString("utf-8"));
+
+    Object.keys(json.components.schemas).forEach((key) => {
+      const JsonSechma = json.components.schemas[key];
+      // 先判断此 sechma 是否因为被其他 sechma 引用已经生成了
+      const mapSechma = map.get(key);
+      if (mapSechma) return;
+
+      function generateSechmaIntoMap(jsonSechma: Sechma, sechmaName: string) {
+        const newSechma: Sechma = {};
+        newSechma.title = jsonSechma.title;
+        newSechma.type = jsonSechma.type;
+        newSechma.description = jsonSechma.description;
+        newSechma.required = jsonSechma.required;
+        function generateProperty() {
+          if (jsonSechma.$ref) {
+            const sem = map.get(refName(jsonSechma.$ref));
+            if (sem) {
+              // 引用的 sechma 不会有ref,且一定是个对象, 为
+              newSechma.properties = sem;
+            } else {
+            }
+          }
+        }
+        map.set(sechmaName, newSechma);
+      }
+    });
+  });
+}
+
+function paseRef(ref: string) {
+  return ref.split("/").filter((i, index) => index !== 0);
+}
+
+function refName(ref: string) {
+  const arr = paseRef(ref);
+  return arr[arr.length - 1];
+}
+
+function getDir() {
+  const p = path.normalize(process.argv[2]);
+  const { root, dir, ext, name } = path.parse(p);
+  return path.join(root ? "" : __dirname, dir, name + ext);
+}
+
+parse(getDir(), process.argv[3]);
---
+++ open-api.d.ts
@@ -0,0 +1,57 @@
+type OpenApi = {
+  openapi: string;
+  info: {
+    title: string;
+    description: string;
+    termsOfService: string;
+    contact: {
+      name: string;
+      url: string;
+      email: string;
+    };
+    version: string;
+  };
+  servers: {
+    url: string;
+    description: string;
+  }[];
+  paths: {
+    [path: string]: Record<"post" | "get" | "delete", PathInstance>;
+  };
+  components: {
+    schemas: {
+      [x: string]: Sechma;
+    };
+  };
+};
+
+type PathInstance = {
+  tags: string[];
+  summary: string;
+  operationId: string;
+  responses: Record<
+    Response["status"],
+    {
+      content: {
+        "*/*": {
+          schema: {
+            $ref: string;
+          };
+        };
+      };
+    }
+  >;
+};
+
+type DataType = "object" | "integer" | "array" | "string";
+
+type Sechma = {
+  title?: string;
+  type?: DataType;
+  description?: string;
+  properties?: Sechma;
+  required?: string[];
+  /** 有ref就说明是一个对象,且没有properties */
+  $ref?: string;
+  [key: string]: Sechma;
+};
